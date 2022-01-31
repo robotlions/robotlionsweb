32250c87bf0cae27ea99377005320ec555aee4bf
robotlions <>
2022-01-30 23:01:43 -0500

    updated with fbase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelopeOpenText} from '@fortawesome/free-solid-svg-icons';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 import logoBarColor from '../src/images/banner1800x300.png';
 import dice1 from '../src/images/slice1c2.jpg';
@@ -9,24 +13,77 @@ import dice2 from '../src/images/slice2c2.jpg';
 import dice3 from '../src/images/slice3c2.jpg';
 import dice4 from '../src/images/slice4c2.jpg';
 
-import {atomicPressCard, storyDiceCard, fallsCard, waterDropCard} from "./Components/Cards";
+
+
+import falls from "../src/images/falls.jpg";
+import waterDrop from "../src/images/waterDrop.jpg";
+import loopFalls from "../src/images/danielLoopFalls.jpg";
+import quarrySteps from "../src/images/quarrySteps.jpg";
+import parkwayFalls from "../src/images/parkwayFalls.jpg";
+import splashBW from "../src/images/splashBW.jpg";
+import wineSplash from "../src/images/wineSplash.jpg";
+import waterRefraction from "../src/images/waterRefraction.jpg";
+
+
+
+
+import { atomicPressCard, storyDiceCard} from "./Components/Cards";
 
 
 
 
 function App() {
 
+  
+
   const [isOpen1, setIsOpen1] = useState(false);
   const [isOpen2, setIsOpen2] = useState(false);
   const [isOpen3, setIsOpen3] = useState(false);
   const [isOpen4, setIsOpen4] = useState(false);
 
+  const [fallsTextVisible, setFallsTextVisible] = useState(false);
+  const [waterDropTextVisible, setWaterDropTextVisible] = useState(false);
+  const [loopFallsTextVisible, setLoopFallsTextVisible] = useState(false);
+  const [quarryStepsTextVisible, setQuarryStepsTextVisible] = useState(false);
+  const [parkwayFallsTextVisible, setParkwayFallsTextVisible] = useState(false);
+  const [splashBWTextVisible, setSplashBWTextVisible] = useState(false);
+  const [wineSplashTextVisible, setWineSplashTextVisible] = useState(false);
+  const [waterRefractionTextVisible, setWaterRefractionTextVisible] = useState(false);
 
+  const fallsCard = <button className="buttonBox" onClick={()=>setFallsTextVisible(!fallsTextVisible)}>
+  {!fallsTextVisible ? <img className="photoImg" alt="water and rocks" src={falls} /> : <p className="photoText">Long exposure on film.</p>}
+  </button>
+  const waterDropCard = <button className="buttonBox" onClick={()=>setWaterDropTextVisible(!waterDropTextVisible)}>
+  {!waterDropTextVisible ? <img className="photoImg" alt="reflection in drop of water" src={waterDrop} /> : <p className="photoText">Digital photo with vintage macro lens.</p>}
+  </button>
+  const loopFallsCard = <button className="buttonBox" onClick={()=>setLoopFallsTextVisible(!loopFallsTextVisible)}>
+  {!loopFallsTextVisible ? <img className="photoImg" alt="waterfall at daniel loop trail" src={loopFalls} /> : <p className="photoText">Daniel Loop Falls in Pisgah National Forest.</p>}
+  </button>
+  const quarryStepsCard = <button className="buttonBox" onClick={()=>setQuarryStepsTextVisible(!quarryStepsTextVisible)}>
+  {!quarryStepsTextVisible ? <img className="photoImg" alt="stone staircase" src={quarrySteps} /> : <p className="photoText">Stone steps in an old rock quarry.</p>}
+  </button>
+  const parkwayFallsCard = <button className="buttonBox" onClick={()=>setParkwayFallsTextVisible(!parkwayFallsTextVisible)}>
+  {!parkwayFallsTextVisible ? <img className="photoImg" alt="waterfall behind trees" src={parkwayFalls} /> : <p className="photoText">Waterfall in North Carolina off the Blue Ridge Parkway.</p>}
+  </button>
+  const splashBWCard = <button className="buttonBox" onClick={()=>setSplashBWTextVisible(!splashBWTextVisible)}>
+  {!splashBWTextVisible ? <img className="photoImg" alt="single drop of water splashing in dark pool" src={splashBW} /> : <p className="photoText">If I were Richard Dreyfuss' character from Close Encounters, water drop photography would be my mountain of mashed potatoes.</p>}
+  </button>
+  const wineSplashCard = <button className="buttonBox" onClick={()=>setWineSplashTextVisible(!wineSplashTextVisible)}>
+  {!wineSplashTextVisible ? <img className="photoImg" alt="red wine splashing into glass" src={wineSplash} /> : <p className="photoText">From a previous life as a wine professional.</p>}
+  </button>
+  const waterRefractionCard = <button className="buttonBox" onClick={()=>setWaterRefractionTextVisible(!waterRefractionTextVisible)}>
+  {!waterRefractionTextVisible ? <img className="photoImg" alt="color background refracting through water drops" src={waterRefraction} /> : <p className="photoText">Goofing around with water and construction paper.</p>}
+  </button>
+  
+  const githubIcon = <FontAwesomeIcon icon={faGithub}/>
+  const envelopeIcon = <FontAwesomeIcon icon={faEnvelopeOpenText}/>
+  const planeIcon = <FontAwesomeIcon icon={faPaperPlane}/>
   
 
   return (
     <motion.div>
     <div className="container g-4">
+      
       <div className="row logoRow">
         <img src={logoBarColor} alt="robot lions"></img></div>
       
@@ -112,11 +169,16 @@ function App() {
             }}
             transition={{ duration: 1.2, ease: [0.04, 0.62, 0.23, 0.98] }}
           >
-            <div className="row contentRow justify-content-center">
-            <p className="textField col-md-8">{fallsCard}
-            <br/>
-            {waterDropCard}
-            </p>
+            <div className="row photosRow contentRow justify-content-center">
+            <p className="col-md-3">{fallsCard}</p>
+            <p className="col-md-3">{waterDropCard}</p>
+            <p className="col-md-3">{loopFallsCard}</p>
+            <p className="col-md-3">{quarryStepsCard}</p>
+            <p className="col-md-3">{parkwayFallsCard}</p>
+            <p className="col-md-3">{splashBWCard}</p>
+            <p className="col-md-3">{wineSplashCard}</p>
+            <p className="col-md-3">{waterRefractionCard}</p>
+
             </div>
           </motion.div>
         )}
@@ -153,7 +215,13 @@ function App() {
         
       </AnimatePresence>
       
+      
       </div>
+      <div className="row brandRow text-end fixed-bottom">
+        <p className="linkLogos">
+        <a id="gitLogo" href="https://github.com/robotlions">{githubIcon}</a>
+        {"\n"}
+        <a id="mailLogo" href="mailto:info@robotlions.com">{planeIcon}</a></p></div>
     </div>
     </motion.div>
   );
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -2,6 +2,18 @@ import pressIcon from "/Users/chadmusick/Code/robotlions/src/images/icon.png";
 import diceLogo from "/Users/chadmusick/Code/robotlions/src/images/dicelogo.png";
 import falls from "/Users/chadmusick/Code/robotlions/src/images/falls.jpg";
 import waterDrop from "/Users/chadmusick/Code/robotlions/src/images/waterDrop.jpg";
+import loopFalls from "/Users/chadmusick/Code/robotlions/src/images/danielLoopFalls.jpg";
+import quarrySteps from "/Users/chadmusick/Code/robotlions/src/images/quarrySteps.jpg";
+import parkwayFalls from "/Users/chadmusick/Code/robotlions/src/images/parkwayFalls.jpg";
+import splashBW from "/Users/chadmusick/Code/robotlions/src/images/splashBW.jpg";
+import wineSplash from "/Users/chadmusick/Code/robotlions/src/images/wineSplash.jpg";
+import waterRefraction from "/Users/chadmusick/Code/robotlions/src/images/waterRefraction.jpg";
+
+
+
+
+
+
 
 
 
@@ -51,31 +63,59 @@ export const storyDiceCard =
                 </div>
 
 
-export const fallsCard =
+// export const fallsCard =
 
-<div className="card">
-    <div className="row g-0">
-        <div className="col-lg-4">
-                <img className="img-fluid" src={falls} alt="jones gap bridge"/></div>
-                <div className="card-body col-md-8">
-                  <h5 className="card-title">Jones Gap</h5>
-                  <p className="card-text">Long exposure on film.<br/>Mamiya RB67 and Ilford HP5</p>
-                </div>
+// <div className="card">
+//     <div className="row g-0">
+//         <div className="col-lg-4">
+//                 <img className="img-fluid" src={falls} alt="jones gap bridge"/></div>
+//                 <div className="card-body col-md-8">
+//                   <h5 className="card-title">Jones Gap</h5>
+//                   <p className="card-text">Long exposure on film.<br/>Mamiya RB67 and Ilford HP5</p>
+//                 </div>
                 
-                </div>
-                </div>
+//                 </div>
+//                 </div>
 
 
-export const waterDropCard =
+// export const waterDropCard =
 
-<div className="card">
-    <div className="row g-0">
-        <div className="col-lg-4">
-                <img className="img-fluid" src={waterDrop} alt="single drop of water"/></div>
-                <div className="card-body col-md-8">
-                  <h5 className="card-title">Reflection in water droplet</h5>
-                  <p className="card-text">Digital<br/>Sony A7 with manual macro lens</p>
-                </div>
+// <div className="card">
+//     <div className="row g-0">
+//         <div className="col-lg-4">
+//                 <img className="img-fluid" src={waterDrop} alt="single drop of water"/></div>
+//                 <div className="card-body col-md-8">
+//                   <h5 className="card-title">Reflection in water droplet</h5>
+//                   <p className="card-text">Digital<br/>Sony A7 with manual macro lens</p>
+//                 </div>
                 
-                </div>
-                </div>
+//                 </div>
+//                 </div>
+
+export const fallsCard = 
+
+  <img className="photoImg" src={falls} />
+  
+
+
+
+  export const waterDropCard =
+  <img className="photoImg" src={waterDrop} alt="image reflected in drop of water"/>
+
+  export const loopFallsCard =
+  <img className="photoImg" src={loopFalls} alt="Daniel Loop waterfall"/>
+
+  export const quarryStepsCard =
+  <img className="photoImg" src={quarrySteps} alt="Stone steps"/>
+
+  export const parkwayFallsCard =
+  <img className="photoImg" src={parkwayFalls} alt="waterfall behind trees"/>
+
+  export const splashBWCard =
+  <img className="photoImg" src={splashBW} alt="single splashing drop of water"/>
+
+  export const wineSplashCard =
+  <img className="photoImg" src={wineSplash} alt="red wine splashing into glass"/>
+
+  export const waterRefractionCard = 
+  <img className="photoImg" src={waterRefraction} alt="bright colors refracted through water"/>
