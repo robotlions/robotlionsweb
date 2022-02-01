223961c50d1d1887bc11f1a71dad84102351f869
robotlions <>
2022-01-31 22:16:34 -0500

    updates card styling

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -3,6 +3,7 @@ import diceLogo from "/Users/chadmusick/Code/robotlions/src/images/dicelogo.png"
 import frontScreen from '/Users/chadmusick/Code/robotlions/src/images/blankPageScreen.png';
 import mainScreen from "/Users/chadmusick/Code/robotlions/src/images/mainPageScreen.png";
 import editScreen from "/Users/chadmusick/Code/robotlions/src/images/editRecipeScreen.png";
+import splashScreen from "/Users/chadmusick/Code/robotlions/src/images/atomicSplash2.jpg";
 
 
 
@@ -18,7 +19,7 @@ export const atomicPressCard =
     <div className="row g-0">
         <div className="col-lg-4">
                 <img className="img-fluid" src={pressIcon} alt="app indicator"/></div>
-                <div className="card-body col-md-8">
+                <div className="card-body d-flex flex-column col-md-8 align-self-center">
                   <h5 className="card-title">Atomic Press</h5>
                   <p className="card-text">React Native app for randomly generating - then editing and saving - recipes for the AeroPress coffee brewer. <br/>Inspired by 1950s atomic design.</p>
                 </div>
@@ -29,16 +30,40 @@ export const atomicPressCard =
 
 
 export const atomicPressDataCards=<div className="row">
+  
   <div className="col-lg-6">
 <div className="card">
   <div className="row">
   <div className="col-7">
-  <img src={frontScreen} className="card-img-top" style={{border: "1px solid"}} alt="app front screen"/>
+  <img src={splashScreen} className="card-img-top" style={{border: "1px solid",borderTopRightRadius: 0}} alt="editing screen"/>
   </div>
-  <div className="col-5">
-  <div className="card-body">
-    <h5 className="card-title">Styling</h5>
-    <p className="card-text">A clean, simple UI inspired by 1950s atomic design.</p>
+  <div className="col-5 d-flex flex-column align-self-center">
+  <div className="card-body" style={{paddingLeft: 0}}>
+    <h5 className="card-title">Boss!</h5>
+    <p className="card-text">Original graphics inspired by 1950s atomic design.</p>
+  </div>
+  </div>
+  </div>
+  </div>
+</div>
+  
+  
+  
+  
+  
+  
+  
+  
+  <div className="col-lg-6">
+<div className="card">
+  <div className="row">
+  <div className="col-7">
+  <img src={frontScreen} className="card-img-top" style={{border: "1px solid",borderTopRightRadius: 0}} alt="app front screen"/>
+  </div>
+  <div className="col-5 d-flex flex-column align-self-center">
+  <div className="card-body" style={{paddingLeft: 0}}>
+    <h5 className="card-title">Keen!</h5>
+    <p className="card-text">A clean, simple but functional UI.</p>
   </div>
   </div>
   </div>
@@ -51,11 +76,11 @@ export const atomicPressDataCards=<div className="row">
 <div className="card">
   <div className="row">
   <div className="col-7">
-  <img src={mainScreen} className="card-img-top" style={{border: "1px solid"}} alt="list of recipes"/>
+  <img src={mainScreen} className="card-img-top" style={{border: "1px solid",borderTopRightRadius: 0}} alt="list of recipes"/>
   </div>
-  <div className="col-5">
-  <div className="card-body">
-    <h5 className="card-title">Browse</h5>
+  <div className="col-5 d-flex flex-column align-self-center">
+  <div className="card-body" style={{paddingLeft: 0}}>
+    <h5 className="card-title">Nifty!</h5>
     <p className="card-text">Scroll through a list of saved, editable recipes.</p>
   </div>
   </div>
@@ -68,17 +93,19 @@ export const atomicPressDataCards=<div className="row">
 <div className="card">
   <div className="row">
   <div className="col-7">
-  <img src={editScreen} className="card-img-top" style={{border: "1px solid"}} alt="editing screen"/>
+  <img src={editScreen} className="card-img-top" style={{border: "1px solid",borderTopRightRadius: 0}} alt="editing screen"/>
   </div>
-  <div className="col-5">
-  <div className="card-body">
-    <h5 className="card-title">Create, Edit and Store Locally</h5>
+  <div className="col-5 d-flex flex-column align-self-center">
+  <div className="card-body" style={{paddingLeft: 0}}>
+    <h5 className="card-title">Classy Chassis!</h5>
     <p className="card-text">Users randomly generate coffee recipes then save locally. No internet connection needed.</p>
   </div>
   </div>
   </div>
   </div>
 </div>
+
+
 
 </div>
 
@@ -115,7 +142,7 @@ export const storyDiceCard =
         <div className="col-lg-4">
           <a href="https://robotlions.github.io/storydice/">
                 <img className="img-fluid" src={diceLogo} alt="game indicator"/></a></div>
-                <div className="card-body col-md-8">
+                <div className="card-body col-md-8 d-flex flex-column align-self-center">
                   <h5 className="card-title">Story Dice</h5>
                   <p  className="card-text">Web-based app written in React to randomly generate story prompts for fiction writers.</p>
                 </div>
