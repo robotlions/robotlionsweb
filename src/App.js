2b691ec7c3dce6d76d3f7f33f8916a581653666d
Chad M <chadmusick@gmail.com>
2023-12-08 09:30:20 -0500

    updates graphics and nav bar, adds bottom nav buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
@@ -9,7 +10,7 @@ import { topNav } from "./Components/Nav";
 import atomicPressGraphic from "./images/atomicPressWebsiteGraphic1.png";
 import modernCoffeeGraphic from "./images/modernCoffeeWebsiteGraphic2.png";
 import dndGraphic from "./images/dnd35WebsiteGraphic2.png";
-import curseGraphic from "./images/curseWebsiteGraphic.png";
+import curseGraphic from "./images/curseWebsiteGraphic3.png";
 
 function App() {
   const githubIcon = <FontAwesomeIcon icon={faGithub} />;
@@ -20,68 +21,131 @@ function App() {
     <div className="container-fluid px-lg-5">
       {topNav}
 
-
-      <div className="card mb-3" style={{maxWidth: "100%"}}>
-  <div className="row g-0">
-    <div className="col-md-3">
-      <img src={atomicPressGraphic} className="img-fluid rounded-start" alt="retro coffee cup on starburst" />
-    </div>
-    <div className="col-md-9">
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+      <div className="card mb-3" style={{ maxWidth: "100%" }}>
+        <div className="row g-0">
+          <div className="col-md-3">
+            <img
+              src={atomicPressGraphic}
+              className="img-fluid rounded-start"
+              alt="retro coffee cup on starburst"
+            />
+          </div>
+          <div className="col-md-9">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">
+                <small className="text-body-secondary">
+                  Last updated 3 mins ago
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
 
-<div className="card mb-3" style={{maxWidth: "100%"}}>
-  <div className="row g-0 flex-row-reverse">
-    <div className="col-md-3">
-      <img src={modernCoffeeGraphic} className="img-fluid rounded-start" alt="1970s orange coffee pot" />
-    </div>
-    <div className="col-md-9">
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+      <div className="card mb-3" style={{ maxWidth: "100%" }}>
+        <div className="row g-0">
+          <div className="col-md-3">
+            <img
+              src={modernCoffeeGraphic}
+              className="img-fluid rounded-start"
+              alt="1970s orange coffee pot"
+            />
+          </div>
+          <div className="col-md-9">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">
+                <small className="text-body-secondary">
+                  Last updated 3 mins ago
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
 
-<div className="card mb-3" style={{maxWidth: "100%"}}>
-  <div className="row g-0">
-    <div className="col-md-3">
-      <img src={dndGraphic} className="img-fluid rounded-start" alt="character sheet dice and pencils" />
-    </div>
-    <div className="col-md-9">
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+      <div className="card mb-3" style={{ maxWidth: "100%" }}>
+        <div className="row g-0">
+          <div className="col-md-3">
+            <img
+              src={dndGraphic}
+              className="img-fluid rounded-start"
+              alt="character sheet dice and pencils"
+            />
+          </div>
+          <div className="col-md-9">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">
+                <small className="text-body-secondary">
+                  Last updated 3 mins ago
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
 
-<div className="card mb-3" style={{maxWidth: "100%"}}>
-  <div className="row g-0 flex-row-reverse">
-    <div className="col-md-3">
-      <img src={curseGraphic} className="img-fluid rounded-start" alt="video game nerds" />
-    </div>
-    <div className="col-md-9">
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+      <div className="card mb-3" style={{ maxWidth: "100%" }}>
+        <div className="row g-0">
+          <div className="col-md-3">
+            <img
+              src={curseGraphic}
+              className="img-fluid rounded-start"
+              alt="video game nerds"
+            />
+          </div>
+          <div className="col-md-9">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">
+                <small className="text-body-secondary">
+                  Last updated 3 mins ago
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
-      
-       
+
+      <div className="row brandRow text-end fixed-bottom">
+        <p className="linkLogos">
+          <a id="gitLogo" href="https://github.com/robotlions">
+            {githubIcon}
+          </a>
+          {"\n"}
+          <a id="mailLogo" href="mailto:info@chadmusick.com">
+            {planeIcon}
+          </a>
+          {"\n"}
+
+          <a id="flickrLogo" href="https://flickr.com/photos/183126740@N02/">
+            {flickrIcon}
+          </a>
+        </p>
+      </div>
+
     </div>
   );
 }
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,20 +6,22 @@ import bannerMobile from "../images/robotLionsMobileBanner.png";
 
 
 
+
 export const topNav = (
 <Navbar bg="white" variant="light" expand="lg" style={{marginBottom:"40px"}}>
       
-        <Navbar.Brand className="navbarBrandBar" href="#home"><img alt="robot lions" className="navLogo" src={bannerMobile} /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand className="navbarBrandBar"><img alt="robot lions" className="navLogo" src={bannerMobile} /></Navbar.Brand>
+        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto align-items-center">
             <Nav.Link href="https://dnd35.robotlions.com/" target="_blank">D&D 3.5 Character Generator</Nav.Link>
             <Nav.Link href="https://goldbox.robotlions.com/" target="_blank">Gold Box Game Editor</Nav.Link>
             
             <Nav.Link href="https://play.google.com/store/apps/developer?id=Robot+Lions" target="_blank">Play Store</Nav.Link>
+
            
           </Nav>
-        </Navbar.Collapse>
+        </Navbar.Collapse> */}
       
     </Navbar>
 )
