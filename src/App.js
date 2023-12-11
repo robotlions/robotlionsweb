1fe664475783d9c1b8ca196e98f68f95f4f9a88a
Chad Musick <chad@funk-o-drome.attlocal.net>
2023-12-11 14:30:00 -0500

    adds about to nav and to app.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import modernCoffeeGraphic from "./images/modernCoffeeWebsiteGraphic.png";
 import dndGraphic from "./images/dnd35WebsiteGraphic3.png";
 import curseGraphic from "./images/curseWebsiteGraphic3.png";
 import googlePlayBadge from "./images/google-play-badge.png";
+import arcadeLogo from "./images/arcadeLogo.png";
+
 
 function App() {
   const githubIcon = <FontAwesomeIcon icon={faGithub} />;
@@ -194,6 +196,47 @@ function App() {
                 >
                   See the code on GitHub
                 </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="spacer"></div>
+
+        <div style={{ textAlign: "center" }}>
+          <h2 className="categoryHeadline" id="about">About</h2>
+        </div>
+
+        <div className="card mb-3" style={{ maxWidth: "100%" }}>
+          <div className="row g-0">
+            <div className="col-md-3">
+              <img
+                src={arcadeLogo}
+                className="img-fluid rounded-start"
+                alt="vintage arcade machine"
+              />
+            </div>
+            <div className="col-md-9">
+              <div className="card-body">
+                <h3 className="card-title">
+                  Robot Lions Apps and Games
+                </h3>
+                <div className="card-text">
+                  <p>Robot Lions is a small developer of niche apps and games.</p>
+                  <p>We pride ourselves on ethical development, which means:<br/>
+                  No pay-to-win. No in-app purchases. No advertising. No selling data. No being gross.</p>
+
+                 
+                </div>
+              </div>
+              <div className="cta-btn">
+                <a
+                  href="mailto:info@robotlions.com"
+                 
+                >
+                  Contact Us
+                </a>
+                
               </div>
             </div>
           </div>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,4 +1,5 @@
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import gradientLogo from "../images/gradientLogoText.png";
 
 export const topNav = (
@@ -10,6 +11,20 @@ export const topNav = (
   >
     <Navbar.Brand className="navbarBrandBar">
       <img alt="robot lions" className="navLogo" src={gradientLogo} />
+    
     </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ms-auto align-items-center">
+        
+
+        <Nav.Link
+        href="#about"
+        >
+         <h4>About</h4>
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
   </Navbar>
 );
